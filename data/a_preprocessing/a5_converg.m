82b9928d226f169215176f57305f1886c3e9ba12
Mirsad Cosovic <mirsad.cosovic@gmail.com>
2019-02-09 19:07:23 +0100

    Add files via upload

---
+++ data/a_preprocessing/a5_converg.m
@@ -0,0 +1,12 @@
+ function [bp] = a5_converg(user, bp)
+    
+
+
+%-----------------------Randomized Damping Messages------------------------
+ bp.idx  = find(bp.Aind);
+ bp.Nmsg = length(bp.idx);
+ temp    = ones(bp.Nmsg, 1);
+ bp.wow  = logical(binornd(temp, user.prob));
+ 
+ bp.pre_time = toc;
+%--------------------------------------------------------------------------
---
+++ data/a_preprocessing/a7_wls.m
@@ -0,0 +1,24 @@
+ function [wls] = a7_wls(data, bp)
+  
+
+ 
+%-----------------Vector of Estimated State Variable-----------------------
+ tic
+ 
+ C = spdiags(data.v, 0, bp.Nfac, bp.Nfac);
+ E = speye(bp.Nfac, bp.Nfac);
+ W = C.^(1/2) \ E;
+
+ Hti = W * data.A;                                                     
+ rti = W * data.b;     
+
+ [Q,R,P] = qr(Hti);                                                         
+ r       = sprank(Hti);                                                       
+ Qn      = Q(:,1:r);                                                           
+ U       = R(1:r,1:r);    
+
+ x     = P * [U \ (Qn' * rti); sparse(bp.Nvar - r, 1)]; 
+ wls.x = full(x);
+ 
+ wls.time = toc; 
+%--------------------------------------------------------------------------
