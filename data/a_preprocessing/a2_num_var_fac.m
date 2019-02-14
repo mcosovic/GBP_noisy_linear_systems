c2ab8d092d75f01e29709b6532b40cd3a10a8d13
Mirsad Cosovic <mirsad.cosovic@gmail.com>
2019-02-14 15:16:07 +0100

    new folder organization

--- data/a_preprocessing/a2_num_var_fac.m
+++
@@ -1,8 +0,0 @@
- function [bp] = a2_num_var_fac(data)
-    
-
- 
-%------------------Number of Variables and Factor Nodes--------------------
- tic
- [bp.Nfac, bp.Nvar] = size(data.A);
-%--------------------------------------------------------------------------
